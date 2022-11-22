47170708f703437fdbec43b9589e89370e69490d
MQ-1175 <85242532+MQ-1175@users.noreply.github.com>
2022-11-22 19:54:02 +0700

    Update and rename main.js to scripts/tmain.js

--- main.js
+++
@@ -1,7 +0,0 @@
-Blocks.atmosphericConcentrator.maxEfficiency = 999999999
-Blocks.carbideCrucible.maxEfficiency = 999999999
-Blocks.surgeCrucible.maxEfficiency = 999999999
-Blocks.cyanogenSynthesizer.maxEfficiency = 999999999
-Blocks.phaseSynthesizer.maxEfficiency = 999999999
-Blocks.malign.maxHeatEfficiency = 999999999
-Blocks.afflict.maxHeatEfficiency = 999999999
---
+++ scripts/tmain.js
@@ -0,0 +1,11 @@
+
+
+Events.on(ClientLoadEvent, b  => {
+Blocks.atmosphericConcentrator.maxEfficiency=999999999
+Blocks.carbideCrucible.maxEfficiency=999999999
+Blocks.surgeCrucible.maxEfficiency=999999999
+Blocks.cyanogenSynthesizer.maxEfficiency=999999999
+Blocks.phaseSynthesizer.maxEfficiency=999999999
+Blocks.malign.maxHeatEfficiency=999999999
+Blocks.afflict.maxHeatEfficiency=999999999
+});
